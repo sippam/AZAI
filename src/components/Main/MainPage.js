5b658b7685a3d14e474f408e38079c9bbd05eb1a
Sippakorn <sippakorn.wo@kkumail.com>
2023-11-28 23:16:06 +0700

    Change main page and have home in navbar

--- src/components/Main/MainPage.js
+++ src/components/Main/MainPage.js
@@ -1,20 +1,64 @@
-import React from "react";
-import logo from "../../logo.svg";
+import React, { useState, useEffect } from "react";
 
 const MainPage = () => {
-  return (
+  const storeText = [
+    "สัมผัสประสบการณ์การเช่าห้องแบบใหม่",
+    "หมดยุคของงานเอกสารบนกระดาษแล้ว",
+  ];
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
-      <div className="w-full flex justify-center">
-        <div className="w-4/5 p-3 border-0 rounded-2xl flex flex-col justify-center items-center mt-10 2xl:w-3/5 xl:w-4/5">
-          <img src={logo} alt="logo" className="w-32 h-32 md:w-64 md:h-64"/>
-          <div className="font-bold text-3xl m-2 md:mb-2">AZAI</div>
-          <div className="mb-2 md:w-3/5 2xl:w-2/5">
-            Lorem ipsum dolor sit amet consectetur. Imperdiet ornare ut quis sed
-            eget vel ipsum. Facilisi potenti proin nisl in arcu porttitor sed
-            quis diam.
-          </div>
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTextIndex((prevIndex) => (prevIndex + 1) % storeText.length);
+    }, 6000); // Change the duration (in milliseconds) as needed
+
+    return () => clearInterval(intervalId);
+  }, []); // Run only once when the component mounts
+
+  return (
+   <div id="home" className="w-full flex justify-center">
+      <div className="w-4/5 p-3 border-0 rounded-2xl flex flex-col justify-center mt-10 xl:w-4/5 2xl:w-3/5">
+        <div className="text-7xl 2xl:text-8xl font-bold md:text-left">AZAI</div>
+        <div className="flex flex-col mt-10 md:text-left">
+          <FadeInOutText
+            key={storeText[currentTextIndex]}
+            text={storeText[currentTextIndex]}
+          />
         </div>
+        <hr className="mt-5 border-2 border-black"/>
       </div>
+    </div>
+  );
+};
+
+const FadeInOutText = ({ text }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    // Show the text with fade-in effect after a short delay
+    const timeoutId1 = setTimeout(() => {
+      setVisible(true);
+    }, 100); // Delay for fade-in effect (adjust as needed)
+
+    // Set a timeout to hide the text with fade-out effect after the fade-in duration
+    const timeoutId2 = setTimeout(() => {
+      setVisible(false);
+    }, 5300); // Adjust the duration as needed
+
+    return () => {
+      clearTimeout(timeoutId1);
+      clearTimeout(timeoutId2);
+    };
+  }, [text]); // Run when the text changes
+
+  return (
+    <div className="w-auto">
+      <div
+        className={`fade-in-out ${visible ? "show" : ""} text-2xl md:text-3xl 2xl:text-5xl font-bold`}
+      >
+        {text}
+      </div>
+    </div>
   );
 };
 
--- src/components/Navbar/Navibar.js
+++ src/components/Navbar/Navibar.js
@@ -8,12 +8,12 @@ const Navibar = () => {
   const navigate = useNavigate();
   const [shadow, setShadow] = useState(false);
 
-
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!isMobileMenuOpen);
   };
 
   const sections = [
+    { id: "home", name: "หน้าหลัก" },
     { id: "about", name: "เกี่ยวกับเรา" },
     { id: "service", name: "บริการ" },
     { id: "contact", name: "ติดต่อเรา" },
@@ -22,16 +22,17 @@ const Navibar = () => {
   const handleNavigation = (id) => {
     if (isMobileMenuOpen) {
       setMobileMenuOpen(false);
+      setOpen(false);
     }
     navigate(`/#${id}`); // Use react-router-dom navigate to handle navigation
 
     setTimeout(() => {
       const targetElement = document.getElementById(id);
-  
+
       if (targetElement) {
         const offset = 70; // Adjust this value as needed
         const targetPosition = targetElement.offsetTop - offset;
-  
+
         window.scrollTo({
           top: targetPosition,
           behavior: "smooth",
@@ -56,37 +57,44 @@ const Navibar = () => {
   return (
     <nav className={`w-full ${isShadow} fixed top-0 bg-white`}>
       <div className="hidden md:block h-12">
-        <div className="flex justify-center h-full">
-          <div className="grid grid-cols-4 h-full w-3/5 items-center">
-            {sections.map((section) => (
+        <div className="flex h-full">
+          {/* <div className="flex items-center h-full my-auto ml-4">
+            <img src={Icon} alt="logo" className="h-10" />
+            <img src={Text} alt="logo" className="h-10" />
+          </div> */}
+          <div className="flex w-full justify-center">
+            <div className="grid grid-cols-5 h-full md:w-4/5 2xl:w-3/5 items-center">
+              {sections.map((section) => (
+                <a
+                  key={section.id}
+                  href={`/#${section.id}`}
+                  className="p-2 block"
+                  onClick={() => handleNavigation(section.id)}
+                >
+                  {section.name}
+                </a>
+              ))}
               <a
-                key={section.id}
-                href={`/#${section.id}`}
+                href="/blog"
                 className="p-2 block"
-                onClick={() => handleNavigation(section.id)}
+                onClick={() => navigate("/blog")}
               >
-                {section.name}
+                บล็อก
               </a>
-            ))}
-            <a
-              href="/blog"
-              className="p-2 block"
-              onClick={() => navigate("/blog")}
-            >
-              บล็อก
-            </a>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Mobile menu button */}
       <div className="flex justify-between md:hidden">
-        <div className="flex items-center h-full my-auto ml-4"></div>
+        <div className="flex items-center h-full my-auto ml-4">
+          {/* <img src={Logo} alt="logo" className="h-10" /> */}
+        </div>
         <div onClick={toggleMobileMenu} className="justify-end">
           <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
       </div>
-      {/* Add mobile menu items here  onClick={() => navigateTo('contact', '/#contact')}*/}
       <div
         className={`md:hidden ${
           isMobileMenuOpen ? "block shadow-md" : "hidden"
